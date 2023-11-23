f552353ca0a7712eff0a36f24af460ad6ba8c26b
Dedeyusuf123 <147039823+Dedeyusuf123@users.noreply.github.com>
2023-11-23 18:16:51 +0700

    Add files via upload

---
+++ tugas6/Pertemuan6 tugas 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	
+	//Mencetak deret angka 10 hingga 100 dengan selilish 10
+	for(i = 10; i <= 100; i += 10)
+	
+	printf ("%d ",i);
+	
+	return 0;
+}
---
+++ tugas6/Pertemuan6 tugas 4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int kecepatan = 2;
+	int waktu = 100;
+	int jarak = kecepatan * waktu;
+	
+	printf ("jarak waktu ditempuh setelah 100 detik: % meter\n", jarak);
+	
+	return 0;
+}
